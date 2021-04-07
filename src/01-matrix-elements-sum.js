a65f4a16ed753d71ff31f9809f9d9b4c7a8db698
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-04-07 11:38:02 +0300

    refactor: whitespaces add

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,18 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      // console.log(matrix[i][j]);
+      sum += matrix[i][j];
+      if (matrix[i][j] === 0 && matrix[i + 1] !== undefined) {
+        sum -= matrix[i + 1][j];
+      }
+    }
+  }
+  return sum;
 }
 
 module.exports = getMatrixElementsSum;
