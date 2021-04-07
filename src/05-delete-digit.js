6656e70ef15bf9905503c6ae12d39c57ae16ec21
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-04-07 21:37:43 +0300

    feat: task 05 complete

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,24 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const initNum = n;
+  let maxNum = 0;
+  let tempNum;
+  let numArr = []; // convert string to a number, then to an array
+  numArr = initNum.toString().split('');
+  for (let i = 0; i < numArr.length; i++) {
+    // iterate array
+    tempNum = numArr[i]; // assign numArr value to a temp variable
+    // tempIndex = i;
+    delete numArr[i]; // exclude element from array
+    if (maxNum <= Number(numArr.join(''))) {
+      // compare array w/o deleted number to maximum
+      maxNum = Number(numArr.join(''));
+    }
+    numArr[i] = tempNum; // get back the deleted number to an array
+  }
+  return maxNum;
 }
 
 module.exports = deleteDigit;
