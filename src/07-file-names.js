8ee61bad8a43dd8f3cdf30235230f895c8c812b1
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-04-08 15:05:30 +0300

    feat: task 07 complete

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,26 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const initArr = names;
+  let counter = 0;
+  let indx;
+  // set up a new RegExp to find (number) part in the end of the array element
+  const Reg = new RegExp(/\(\d\)+$/gm);
+  for (let i = 0; i < initArr.length; i++) {
+    // do add (counter) to the elemets while there are matching elements in the array
+    while (initArr.indexOf(initArr[i], i + 1) !== -1) {
+      // to distinguish true (number) from false (number) lets do this, if true, reset counter
+      if (Reg.test(initArr[i].toString())) {
+        counter = 0;
+      }
+      // iterate counter if elements match
+      counter++;
+      indx = initArr.indexOf(initArr[i], i + 1);
+      initArr[indx] = `${initArr[indx]}(${counter})`;
+    }
+  }
+  return initArr;
 }
 
 module.exports = renameFiles;
