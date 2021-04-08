6f34c88d936ea27de0e56560d08e795ea24a5903
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-04-08 21:47:44 +0300

    feat: task 10 draft complee

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -13,18 +13,22 @@
  */
 function findIndex(array, value) {
   const arr = array;
-  let searchValue = value;
-  let maxElem = arr.length -1;
+  const searchValue = value;
+  let maxElem = arr.length - 1;
   let minElem = 0;
-  let midElem = Math.floor((maxElem + minElem) / 2);
+  let midElem;
   let found = false;
-
-  while ((found === false && minElem <= maxElem)) {
+  let searchIndex;
+  // do binary search while shwitcher is false and array length more than 1 element
+  while (found === false && minElem <= maxElem) {
+    // divide array by half
+    midElem = Math.floor((maxElem + minElem) / 2);
+    // compare elements - midElem match we need? switch to 'true'
     if (arr[midElem] === searchValue) {
       found = true;
-      searchIndex = midElem;
+      searchIndex = midElem; // determine if our element in the beggining of the array
     } else if (arr[midElem] > searchValue) {
-      maxElem = midElem - 1;
+      maxElem = midElem - 1; // determine if our element in the end of the array
     } else if (arr[midElem] < searchValue) {
       minElem = midElem + 1;
     }
