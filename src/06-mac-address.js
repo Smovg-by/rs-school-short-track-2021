bc2b45b3f85c201b69ce08d59c015922ab774417
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-04-08 12:01:05 +0300

    feat: task 06 complete

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,24 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  // check if the length in 17 characters long
+  const str = n;
+  if (str.length !== 17) {
+    return false;
+  }
+  // check '-' symbol after pair of letter/number
+  for (let i = 2; i < str.length; i += 3) {
+    if (str[i] !== '-') {
+      return false;
+    }
+  }
+  // check if the string consists only of A-F, 0-9 and '-' symbols
+  const Reg = new RegExp(/[^A-F0-9-]/);
+  if (Reg.test(str)) {
+    return false;
+  }
+  return true;
 }
 
 module.exports = isMAC48Address;
