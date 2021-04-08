8ad9126139b241491e110617a2a44b9273149cf3
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-04-08 16:58:53 +0300

    feat: task 09 complete

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,18 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  let num = n;
+  num = num.toString();
+  let acc = 0;
+  for (let i = 0; i < num.length; i++) {
+    acc += Number(num[i]);
+  }
+  // if we got more than 1 digit, call the function again (recursion)
+  if (acc.toString().length > 1) {
+    return getSumOfDigits(acc);
+  }
+  return acc;
 }
 
 module.exports = getSumOfDigits;
