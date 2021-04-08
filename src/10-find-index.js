5f62fe76ff6d232904291b9e0b2dc40c5853aaca
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-04-08 18:10:19 +0300

    feat: task 10 draft !!!complee

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,25 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  const arr = array;
+  let searchValue = value;
+  let maxElem = arr.length -1;
+  let minElem = 0;
+  let midElem = Math.floor((maxElem + minElem) / 2);
+  let found = false;
+
+  while ((found === false && minElem <= maxElem)) {
+    if (arr[midElem] === searchValue) {
+      found = true;
+      searchIndex = midElem;
+    } else if (arr[midElem] > searchValue) {
+      maxElem = midElem - 1;
+    } else if (arr[midElem] < searchValue) {
+      minElem = midElem + 1;
+    }
+  }
+  return searchIndex;
 }
 
 module.exports = findIndex;
