a6e6c1d1b6c383e93458253f1288918fe429ddea
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-04-09 10:30:53 +0300

    feat: task 11 draft complee

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,28 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const initString = str;
+  let counter = 1;
+  const codeArr = [];
+  let codeStr = '';
+  for (let i = 1; i < initString.length + 1; i++) {
+    // iterate string. length + 1 add to avoid 'last character error',
+    // when we have nothing to compare the last symbol with
+    if (initString[i] === initString[i - 1]) {
+      // plus counter is previous character match the currrent one
+      counter++;
+    } else if (initString[i] !== initString[i - 1]) {
+      // if previous character does not match current push counter if it is >1 and push character
+      if (counter > 1) {
+        codeArr.push(counter);
+      }
+      codeArr.push(initString[i - 1]);
+      counter = 1;
+    }
+  } // join the array to get the string
+  codeStr = codeArr.join('');
+  return codeStr;
 }
 
 module.exports = encodeLine;
