aa84b343b1b75e33137593d98c6d054652c7dabc
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-04-09 11:37:29 +0300

    feat: task 15 complee

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,18 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const emailString = email;
+  let domainArr = [];
+  // create new regular expression: /(?<=\w@).*?(?=$)/gm
+  // (?<=\w@) - look behind thhe string for matching any word char \w and symbol @
+  // '.' - any character, '*?' - repeated unlimited times
+  // (?=$) - look ahead to the position $ - 'end of the line'
+  const reg = new RegExp(/(?<=\w@).*?(?=$)/gm);
+  // get the array with matching result.
+  domainArr = reg.exec(emailString);
+  // Element '0' of .exec() array contains the matching strine - domain name
+  return domainArr[0];
 }
 
 module.exports = getEmailDomain;
