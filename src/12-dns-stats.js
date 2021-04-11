e67aa531cc50b49ec5499aa7b3f8a52909a8be9f
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-04-11 21:43:04 +0300

    feat: task 12 draft complete, refactoring required!

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,50 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  let domainsArray = [];
+  domainsArray = [...domains]; // clone array
+  const domStatsObj = {}; // create new object
+  if (domainsArray.length === 0) {
+    // if there is nothing to maintain - return empty object
+    return domStatsObj;
+  }
+  let counter = 0;
+  let addStr = '';
+  // add a '.' to the begginning of each array item (to find words boundaries than)
+  for (let j = 0; j < domainsArray.length; j++) {
+    domainsArray[j] = `.${domainsArray[j]}`;
+  }
+  // add the first property to the object, We need it to compare strings later
+  let tempStr = domainsArray[0];
+  let pointPos = tempStr.lastIndexOf('.');
+  let strPartTemp = tempStr.substring(pointPos, tempStr.length);
+  // cut the array item string until the length > 0
+  while (domainsArray.join('').length !== 0) {
+    for (let i = 0; i < domainsArray.length; i++) {
+      if (domainsArray[i].length > 0) {
+        tempStr = domainsArray[i];
+        pointPos = tempStr.lastIndexOf('.');
+        const strPart = tempStr.substring(pointPos, tempStr.length);
+        // if previous property KEY differ from current,
+        // reset counter and remember perevious KEY name
+        if (strPartTemp !== strPart) {
+          counter = 1;
+          addStr += strPartTemp;
+          strPartTemp = strPart;
+        } else {
+          counter++;
+        }
+        // add a KEY and VALUE to the object
+        domStatsObj[addStr + strPart] = counter;
+        // cut the current string until next '.'
+        domainsArray[i] = domainsArray[i].substring(0, pointPos);
+      }
+    }
+    // reset counter after iteration
+    counter = 0;
+  }
+  return domStatsObj;
 }
 
 module.exports = getDNSStats;
